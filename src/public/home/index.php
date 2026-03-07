17cb8ff909acbfdd9e33b2c25a01b647e6680376
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-07 18:04:20 +0800

    ✨ feat(home): add initial layout for main panel

---
+++ src/public/home/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listen with Apollo</title>
+    <link rel="stylesheet" href="/global.css">
+</head>
+<body>
+    <!-- MAIN PANEL -->
+    <div class="p-3 bg-panel">
+        <!-- TOP PICK SONG CARDS -->
+        <div class="mb-8">
+            <h1 class="text-[2rem] font-bold">Top Picks</h1>
+            <div class="flex space-x-2">
+                <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                    <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                        <img 
+                            src="/assets/icons/music-note.svg" 
+                            alt="music"
+                            class="w-full h-full"
+                        />
+                    </div>
+                    <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                    <p class="text-sm font-light">Chano the Artist</p>
+                </div>
+
+                <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                    <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                        <img 
+                            src="/assets/icons/music-note.svg" 
+                            alt="music"
+                            class="w-full h-full"
+                        />
+                    </div>
+                    <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                    <p class="text-sm font-light">Chano the Artist</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- RECOMMENDED ARTIST CARDS -->
+        <div>
+            <h1 class="text-[2rem] font-bold"">Recommended Artist</h1>
+        </div>
+    </div>
+</body>
+</html>
