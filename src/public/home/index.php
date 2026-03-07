fb598ee31580dc49a56005a31e981d380de159ff
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-07 18:38:03 +0800

    ✨ feat(sidebar): add sidebar for navigating between features

--- src/public/home/index.php
+++ src/public/home/index.php
@@ -7,42 +7,97 @@
     <link rel="stylesheet" href="/global.css">
 </head>
 <body>
-    <!-- MAIN PANEL -->
-    <div class="p-3 bg-panel">
-        <!-- TOP PICK SONG CARDS -->
-        <div class="mb-8">
-            <h1 class="text-[2rem] font-bold">Top Picks</h1>
-            <div class="flex space-x-2">
-                <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
-                    <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
-                        <img 
-                            src="/assets/icons/music-note.svg" 
-                            alt="music"
-                            class="w-full h-full"
-                        />
-                    </div>
-                    <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
-                    <p class="text-sm font-light">Chano the Artist</p>
-                </div>
+    <div class="flex flex-col space-y-3">
+        <!-- HERO -->
+        <nav class="flex justify-between items-center px-6 py-4 bg-panel">
+            <h1 class="text-[1.5rem] font-semibold">Apollo</h1>
+            <ul class="flex space-x-6 text-[1.125rem]">
+                <li>Documentation</li>
+                <li>GitHub</li>
+                <li>Creator</li>
+            </ul>
+        </nav>
 
-                <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
-                    <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
-                        <img 
-                            src="/assets/icons/music-note.svg" 
-                            alt="music"
-                            class="w-full h-full"
-                        />
-                    </div>
-                    <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
-                    <p class="text-sm font-light">Chano the Artist</p>
+        <!-- LOWER -->
+        <div class="flex pr-3 space-x-3">
+            <!-- SIDE BAR -->
+            <div class="flex flex-col pl-3 pr-6 py-4 space-y-2 bg-panel text-secondary text-[1.125rem] rounded-tr-lg">
+                <div>
+                    <h1>Search</h1>
+                </div>
+                <div>
+                    <h1>Library</h1>
+                </div>
+                <div>
+                    <h1>Explore</h1>
                 </div>
             </div>
+
+            <!-- MAIN PANEL -->
+            <div class="w-full p-3 bg-panel rounded-lg">
+                <!-- TOP PICK SONG CARDS -->
+                <div class="mb-8">
+                    <h1 class="text-[2rem] font-bold">Top Picks</h1>
+                    <div class="flex space-x-2">
+                        <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                            <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                                <img 
+                                    src="/assets/icons/music-note.svg" 
+                                    alt="music"
+                                    class="w-full h-full"
+                                />
+                            </div>
+                            <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                            <p class="text-sm font-light">Chano the Artist</p>
+                        </div>
+
+                        <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                            <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                                <img 
+                                    src="/assets/icons/music-note.svg" 
+                                    alt="music"
+                                    class="w-full h-full"
+                                />
+                            </div>
+                            <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                            <p class="text-sm font-light">Chano the Artist</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- RECOMMENDED ARTIST CARDS -->
+                <div class="mb-8">
+                    <h1 class="text-[2rem] font-bold">Recommended Artist</h1>
+                    <div class="flex space-x-2">
+                        <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                            <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                                <img 
+                                    src="/assets/icons/music-note.svg" 
+                                    alt="music"
+                                    class="w-full h-full"
+                                />
+                            </div>
+                            <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                            <p class="text-sm font-light">Chano the Artist</p>
+                        </div>
+
+                        <div class="flex flex-col p-3 min-w-48 bg-card border border-border rounded-lg">
+                            <div class="w-full mb-2 max-h-48 bg-panel rounded-md">
+                                <img 
+                                    src="/assets/icons/music-note.svg" 
+                                    alt="music"
+                                    class="w-full h-full"
+                                />
+                            </div>
+                            <h2 class="text-[1.125rem] font-semibold">Hello, World</h2>
+                            <p class="text-sm font-light">Chano the Artist</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
-        <!-- RECOMMENDED ARTIST CARDS -->
-        <div>
-            <h1 class="text-[2rem] font-bold"">Recommended Artist</h1>
-        </div>
     </div>
 </body>
 </html>
