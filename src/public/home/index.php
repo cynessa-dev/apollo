8b8f7c8b744cbf6f86c0638e10e44e0c1c493205
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-07 18:58:20 +0800

    ✨ feat(sidebar): add icons to features for better clarity

--- src/public/home/index.php
+++ src/public/home/index.php
@@ -27,14 +27,24 @@
         <!-- LOWER -->
         <div class="flex pr-3 space-x-3">
             <!-- SIDE BAR -->
-            <div class="flex flex-col pl-3 pr-6 py-4 space-y-2 bg-panel text-secondary text-[1.125rem] rounded-tr-lg">
-                <div>
+            <div class="flex flex-col pl-3 pr-6 py-4 space-y-6 bg-panel text-secondary text-[1.125rem] rounded-tr-lg">
+                <div class="flex items-center space-x-3">
+                    <svg class="text-body w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
                     <h1>Search</h1>
                 </div>
-                <div>
+                <div class="flex items-center space-x-3">
+                    <svg class="text-body w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+                        <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5z"/>
+                    </svg>
                     <h1>Library</h1>
                 </div>
-                <div>
+                <div class="flex items-center space-x-3">
+                    <svg class="text-body w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-compass" viewBox="0 0 16 16">
+                        <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016m6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0"/>
+                        <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+                    </svg>
                     <h1>Explore</h1>
                 </div>
             </div>
