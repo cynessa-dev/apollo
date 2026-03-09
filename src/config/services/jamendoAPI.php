1c8b7dca4cd3850a9ea608ac0f246775b413966a
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-09 14:59:45 +0800

    ✨ feat(jamendo): add jamendo client id to enable the service

--- src/config/services/jamendoAPI.php
+++ src/config/services/jamendoAPI.php
@@ -2,7 +2,11 @@
 
 class JamendoAPI {
 
-    private $client_id = 'CLIENT_ID';
+    private $client_id;
+
+    public function __construct() {
+        $this->client_id = $_ENV['JAMENDO_CLIENT_ID'];
+    }
 
     public function searchTracks($query) {
         $url = 'https://api.jamendo.com/v3.0/tracks/?client_id='
