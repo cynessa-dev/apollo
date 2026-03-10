e36e6376ffc4089d78ee49deed6ba4c0a5635150
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-03-10 10:36:22 +0800

    ✨ feat(player): update player.js to add pause and resume capabilities

--- src/config/scripts/player.js
+++ src/config/scripts/player.js
@@ -1,16 +1,27 @@
 
 const player = document.getElementById('player');
 
+let action = 'play'
+let currentTrack = null;
+
 function playTrack(url) {
+
+    // Compare current and incoming tracks, it shows what the user is trying to do and avoids reloading the same track repeatedly
+    if (currentTrack === url) {
+        player.paused ? player.play() : player.pause();
+
+        return;
+    }
+
+    // Encode the url to avoid errors when attaching to the api
     const enconded = encodeURIComponent(url);
-    
-    fetch(`/config/backend/api/player.php?action=play&track=${enconded}`)
+
+    fetch(`/config/backend/api/player.php?action=${action}&track=${enconded}`)
         .then(res => res.json())
         .then(data => {
-            
-            if (data.status === "playing") {
-                player.src = data.track;
-                player.play();
-            }
+
+            currentTrack = url; // Save the incoming track for future comparing
+            player.src = data.track;
+            player.play();
         });
 }
